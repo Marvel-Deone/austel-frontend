04cb5f984747b9d1512723fdc8f349e884db878f
Kelvin Ajayi <89777325+Marvel-Deone@users.noreply.github.com>
2022-11-01 07:55:16 +0000

    Edited

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Dashboard from './pages/tenants/Dashboard';
+import Login from './pages/tenants/Login';
+import Payment from './pages/tenants/Payment';
+import Signup from './pages/tenants/Signup';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+     <BrowserRouter>
+        <Routes>
+          <Route path='' element={<Signup/>}/>
+          <Route path='/tenant/login' element={<Login />}></Route>
+          <Route path='/tenant/dashboard' element={<Dashboard/>}></Route>
+          <Route path='/tenant/payment' element={<Payment/>}></Route>
+        </Routes>
+      </BrowserRouter>
+  </>
   );
 }
 
